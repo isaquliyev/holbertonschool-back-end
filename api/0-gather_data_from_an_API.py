f08c8e004926f1ce1ea672ee27c2998ad28e86c8
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-22 21:47:08 +0400

    Module documented and import issue fixed

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 
+"""
+    The `Gather data from an API` module
+"""
 
-import requests
-from sys import argv
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
 
 
-user_name = requests.get(f"https://jsonplaceholder.\
+    user_name = requests.get(f"https://jsonplaceholder.\
 typicode.com/users?id={argv[1]}").json()[0]['name']
 
-todos = requests.get(f"https://jsonplaceholder.typicode.\
+    todos = requests.get(f"https://jsonplaceholder.typicode.\
 com/todos?userId={argv[1]}").json()
 
-total = len(todos)
-completed = 0
-titles = ""
-for i in todos:
-    if i['completed']:
-        completed += 1
-        titles += "\t" + i['title'] + "\n"
+    total = len(todos)
+    completed = 0
+    titles = ""
+    for i in todos:
+        if i['completed']:
+            completed += 1
+            titles += "\t" + i['title'] + "\n"
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(user_name, completed, total))
-print(titles[:-1])
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, completed, total))
+    print(titles[:-1])
