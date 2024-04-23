8b9b7de0c6825de430c8fd29cd114f886e730a96
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-23 16:27:11 +0400

    Dictionary of list of dictionaries

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    `Export to JSON` module
+"""
+
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+
+    new_dict = {}
+    new_list = []
+
+    user_list = requests.get(f"https://jsonplaceholder.\
+typicode.com/users/").json()
+
+    for user in user_list:
+        todos = requests.get(f"https://jsonplaceholder.typicode.\
+com/todos?userId={user.get('id')}").json()
+        for todo in todos:
+            new_list.append({'username': user.get('username'),
+                             'task': todo.get('title'),
+                             'completed': todo.get('completed')})
+        new_dict[user.get('id')] = new_list
+    json.dump(new_dict, open(f"todo_all_employees.json", "w"))
